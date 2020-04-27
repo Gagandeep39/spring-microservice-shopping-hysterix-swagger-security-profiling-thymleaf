ab8fc43d72d0e5cd2744fe7b130808ee1ae2dbec
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-27 21:08:38 +0530

    Excluded /actuator and /hystrix from security

--- shopping2-login-microservice/src/main/java/com/gagan/shopping2loginmicroservice/config/SecurityConfig.java
+++ shopping2-login-microservice/src/main/java/com/gagan/shopping2loginmicroservice/config/SecurityConfig.java
@@ -1,5 +1,13 @@
-package com.gagan.shopping2loginmicroservice.config;
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-04-27 21:06:46
+ * @modify date 2020-04-27 21:06:46
+ * @desc [description]
+ */
 
+
+package com.gagan.shopping2loginmicroservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -7,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -45,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
                 .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers(  "/RegisterCtrl/**", "/error", "/h2-console/**").permitAll()
+                .antMatchers(  "/RegisterCtrl/**", "/error", "/h2-console/**", "/hystrix/**", "/actuator/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
